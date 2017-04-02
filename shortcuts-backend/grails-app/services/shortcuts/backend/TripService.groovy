99e8ee7345d6295cc728c7195c09aa0ad8a7fb49
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-04-02 14:21:00 +0200

    Refactor trip creation

--- shortcuts-backend/grails-app/services/shortcuts/backend/TripService.groovy
+++ shortcuts-backend/grails-app/services/shortcuts/backend/TripService.groovy
@@ -8,23 +8,11 @@ class TripService {
     DateService dateService
 
     def createTrip(Long routeId, tripJson) {
-        if (!routeId) {
-            throw new TripCreationException('Trip requires a specified route')
-        }
         def path = tripJson.path
-        if (!path) {
-            throw new TripCreationException('Trip requires a specified path')
-        }
-
-        Route route = Route.get(routeId)
-        def (Date startTime, Date endTime) = getStartAndEndTime(path)
-
-        Trip trip = new Trip(route: route, startTime: startTime, endTime: endTime)
-        path.each { it ->
-            trip.addToPath(new TripPoint(latitude: it.latitude, longitude: it.longitude, time: dateService.parseDate(it.time)))
-        }
+        validateTripData(routeId, path)
+        Trip trip = buildTrip(routeId, path)
+        addTripPoints(trip, path)
         trip.save(flush: true, failOnError: true)
-
         return trip
     }
 
@@ -36,6 +24,21 @@ class TripService {
         return Trip.get(tripId)
     }
 
+    private static def validateTripData(Long routeId, path) {
+        if (!routeId) {
+            throw new TripCreationException('Trip requires a specified route')
+        }
+        if (!path) {
+            throw new TripCreationException('Trip requires a specified path')
+        }
+    }
+
+    private Trip buildTrip(Long routeId, path) {
+        Route route = Route.get(routeId)
+        def (Date startTime, Date endTime) = getStartAndEndTime(path)
+        return new Trip(route: route, startTime: startTime, endTime: endTime)
+    }
+
     private def getStartAndEndTime(path) {
         def timeInstances = path.collect {
             dateService.parseDate(it.time)
@@ -43,5 +46,12 @@ class TripService {
         return [timeInstances.min(), timeInstances.max()]
     }
 
+    private def addTripPoints(Trip trip, path) {
+        path.each { it ->
+            Date time = dateService.parseDate(it.time)
+            TripPoint point = new TripPoint(latitude: it.latitude, longitude: it.longitude, time: time)
+            trip.addToPath(point)
+        }
+    }
 
 }
