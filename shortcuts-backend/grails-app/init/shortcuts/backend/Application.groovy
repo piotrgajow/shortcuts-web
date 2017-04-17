95ce8ce69c919c9462cd32ff45cf9fd883bd717e
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-04-17 21:32:58 +0200

    Add JSON mapping of joda time period class

--- shortcuts-backend/grails-app/init/shortcuts/backend/Application.groovy
+++ shortcuts-backend/grails-app/init/shortcuts/backend/Application.groovy
@@ -2,9 +2,12 @@ package shortcuts.backend
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.converters.JSON
+import org.joda.time.Period
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+        JSON.registerObjectMarshaller(Period, new PeriodJsonMapper().&mapToJson)
     }
 }
