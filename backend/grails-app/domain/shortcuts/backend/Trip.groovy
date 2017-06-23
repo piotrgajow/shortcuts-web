3aa48a377a7652a3c99c2ec5b9a182c401cbf530
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-06-23 22:09:19 +0100

    Refactor domain classes
    - Switch from implemented hashCode and equals methods to generated ones
    via adnotation

--- backend/grails-app/domain/shortcuts/backend/Trip.groovy
+++ backend/grails-app/domain/shortcuts/backend/Trip.groovy
@@ -1,8 +1,10 @@
 package shortcuts.backend
 
+import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
 @ToString(includeNames = true)
+@EqualsAndHashCode
 class Trip {
 
     Route route
@@ -24,24 +26,4 @@ class Trip {
         duration nullable: false
     }
 
-    boolean equals(o) {
-        if (this.is(o)) return true
-        if (!(o instanceof Trip)) return false
-
-        Trip trip = (Trip) o
-
-        if (id != trip.id) return false
-        if (route != trip.route) return false
-        if (startTime != trip.startTime) return false
-        return (duration == trip.duration)
-    }
-
-    int hashCode() {
-        int result
-        result = (route != null ? route.hashCode() : 0)
-        result = 31 * result + (startTime != null ? startTime.hashCode() : 0)
-        result = 31 * result + (duration != null ? duration.hashCode() : 0)
-        result = 31 * result + (id != null ? id.hashCode() : 0)
-        return result
-    }
 }
