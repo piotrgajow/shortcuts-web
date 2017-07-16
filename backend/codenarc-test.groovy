57e82bce1c3aa365110160761bd75dd2d271c430
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-07-16 13:25:21 +0200

    Add more rules to CodeNarc test

--- backend/codenarc-test.groovy
+++ backend/codenarc-test.groovy
@@ -1,6 +1,30 @@
 ruleset {
     ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/concurrency.xml')
+//    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/enhanced.xml')
+    ruleset('rulesets/exceptions.xml') {
+        exclude 'ThrowRuntimeException'
+    }
+    ruleset('rulesets/formatting.xml') {
+        exclude 'ClassJavadoc'
+        exclude 'SpaceAroundMapEntryColon'
+        LineLength {
+            length = 200
+        }
+    }
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/grails.xml')
+    ruleset('rulesets/convention.xml') {
+        exclude 'NoDef'
+    }
+    ruleset('rulesets/design.xml')
     ruleset('rulesets/groovyism.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/jdbc.xml')
+    ruleset('rulesets/junit.xml')
+    ruleset('rulesets/logging.xml')
     ruleset('rulesets/naming.xml') {
         FactoryMethodName {
             regex = /(build.*|make.*)/
@@ -9,9 +33,11 @@ ruleset {
             doNotApplyToClassNames= '*Spec'
         }
     }
-    ruleset('rulesets/unused.xml')
+    ruleset('rulesets/security.xml')
+    ruleset('rulesets/serialization.xml')
+    ruleset('rulesets/size.xml')
     ruleset('rulesets/unnecessary.xml') {
         exclude 'UnnecessaryReturnKeyword'
     }
-    ruleset('rulesets/grails.xml')
+    ruleset('rulesets/unused.xml')
 }
