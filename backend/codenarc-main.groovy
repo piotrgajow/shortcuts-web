82d3be7166ba5387e841f94586613524727e5220
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-07-16 02:01:32 +0200

    Extend main codenarc ruleset

--- backend/codenarc-main.groovy
+++ backend/codenarc-main.groovy
@@ -1,14 +1,35 @@
 ruleset {
     ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/concurrency.xml')
+    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/enhanced.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/formatting.xml') {
+        exclude 'ClassJavadoc'
+        exclude 'SpaceAroundMapEntryColon'
+    }
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/grails.xml')
+    ruleset('rulesets/convention.xml') {
+        exclude 'NoDef'
+    }
+    ruleset('rulesets/design.xml')
     ruleset('rulesets/groovyism.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/jdbc.xml')
+    ruleset('rulesets/junit.xml')
+    ruleset('rulesets/logging.xml')
     ruleset('rulesets/naming.xml') {
         FactoryMethodName {
             regex = /(build.*|make.*)/
         }
     }
-    ruleset('rulesets/unused.xml')
+    ruleset('rulesets/security.xml')
+    ruleset('rulesets/serialization.xml')
+    ruleset('rulesets/size.xml')
     ruleset('rulesets/unnecessary.xml') {
         exclude 'UnnecessaryReturnKeyword'
     }
-    ruleset('rulesets/grails.xml')
+    ruleset('rulesets/unused.xml')
 }
