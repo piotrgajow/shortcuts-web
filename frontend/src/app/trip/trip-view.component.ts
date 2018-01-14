4135e59dff1ba9b45b8e6ef38be762788b9cd6da
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-14 20:37:44 +0100

    Rework trip component

---
+++ frontend/src/app/trip/trip-view.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Observable, Subscription } from 'rxjs/Rx';
+
+import { Trip } from './trip';
+import { TripService } from './trip.service';
+
+@Component({
+    templateUrl: './trip-view.component.html'
+})
+export class TripViewComponent implements OnInit, OnDestroy {
+    static readonly TIMER_DELAY = 0;
+    static readonly TIMER_INTERVAL= 1000;
+
+    trip: Trip;
+    timerSubscription: Subscription;
+
+    constructor(
+        private router: Router,
+        private tripService: TripService,
+    ) {
+        this.trip = new Trip();
+        this.trip.startTime = new Date();
+        this.trip.time = 0;
+    }
+
+    ngOnInit(): void {
+        const timer = Observable.timer(TripViewComponent.TIMER_DELAY, TripViewComponent.TIMER_INTERVAL);
+        this.timerSubscription = timer.subscribe(t => {
+            this.trip.time = t;
+        });
+    }
+
+    finalize(): void {
+        this.tripService.currentTrip = this.trip;
+        this.router.navigateByUrl('/selectRoute').then();
+    }
+
+    ngOnDestroy(): void {
+        this.timerSubscription.unsubscribe();
+    }
+
+}
