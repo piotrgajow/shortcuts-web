42119db6288cf3e4d0ae8cd1381ad905e2d4eabf
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-14 20:28:17 +0100

    Add style to trip view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatCardModule } from '@angular/material';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { TimePipe } from './shared/time.pipe';
     HttpModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
     BrowserAnimationsModule,
   ],
   providers: [
