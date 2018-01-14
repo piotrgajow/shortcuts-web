49e6022b4cbc6cf7ef4167875f27289f0ac5b5a7
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-14 18:25:19 +0100

    Add material design module and styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,18 +26,22 @@ import { TimePipe } from './shared/time.pipe';
     RouteSelectorComponent,
     TimerComponent,
     TripComponent,
-    TimePipe
+    TimePipe,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
   ],
   providers: [
-      RouteService,
-      TripService,
+    RouteService,
+    TripService,
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
