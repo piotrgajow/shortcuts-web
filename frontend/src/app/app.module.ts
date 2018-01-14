ce617b82cfbb0be2448515af091a7bfbd04ec4f3
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-01-14 20:41:27 +0100

    Change default code indent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,37 +13,35 @@ import { RouteFormComponent } from './route/form/route-form.component';
 import { RouteListComponent } from './route/list/route-list.component';
 import { RouteSelectorComponent } from './route/selector/route-selector.component';
 import { RouteService } from './route/route.service';
-import { TimerComponent } from './timer/timer.component';
-import { TripComponent } from './trip/trip.component';
+import { TripViewComponent } from './trip/trip-view.component';
 import { TripService } from './trip/trip.service';
 import { TimePipe } from './shared/time.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainMenuComponent,
-    RouteFormComponent,
-    RouteListComponent,
-    RouteSelectorComponent,
-    TimerComponent,
-    TripComponent,
-    TimePipe,
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [
-    RouteService,
-    TripService,
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainMenuComponent,
+        RouteFormComponent,
+        RouteListComponent,
+        RouteSelectorComponent,
+        TripViewComponent,
+        TimePipe,
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [
+        RouteService,
+        TripService,
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
