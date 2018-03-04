0d89e028f8ce1d852d1b419a9ef0d2dd3d261462
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-03-04 23:28:21 +0100

    Fix lint issues in app component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed, ComponentFixture, async, } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
@@ -10,13 +10,13 @@ describe('AppComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            schemas: [
-                CUSTOM_ELEMENTS_SCHEMA,
-            ],
             declarations: [
                 AppComponent,
             ],
-        }).compileComponents();
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA,
+            ],
+        }).compileComponents().then().catch();
     }));
 
     beforeEach(() => {
