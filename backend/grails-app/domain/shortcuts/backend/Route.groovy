a2c44a7992ad99833c6b675c67e4a8f53b220d47
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-03-15 00:36:50 +0100

    Add location from and to to domain classes

--- backend/grails-app/domain/shortcuts/backend/Route.groovy
+++ backend/grails-app/domain/shortcuts/backend/Route.groovy
@@ -2,23 +2,30 @@ package shortcuts.backend
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
+import util.Constraints
 
 @ToString(includeNames = true)
 @EqualsAndHashCode
 class Route {
 
     Long id
+    String locationFrom
+    String locationTo
     String description
 
     static mapping = {
         table 'route'
 
         id column: 'route_id'
+        locationFrom column: 'location_from'
+        locationTo column: 'location_to'
         description column: 'description'
     }
 
     static constraints = {
-        description nullable: false, blank: false
+        locationFrom Constraints.NOT_EMPTY_STRING
+        locationTo Constraints.NOT_EMPTY_STRING
+        description Constraints.NOT_EMPTY_STRING
     }
 
 }
--- backend/src/main/groovy/util/Constraints.groovy
+++ backend/src/main/groovy/util/Constraints.groovy
@@ -3,4 +3,5 @@ package util
 class Constraints {
 
     static final Map NOT_NULL = [nullable: false]
+    static final Map NOT_EMPTY_STRING = [nullable: false, blank: false]
 }
