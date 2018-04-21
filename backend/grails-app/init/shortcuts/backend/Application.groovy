5ee6349fe7b78dde7c18784233b12802ff3458f2
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-04-21 22:57:40 +0200

    Add loading external backend configuration

--- backend/grails-app/init/shortcuts/backend/Application.groovy
+++ backend/grails-app/init/shortcuts/backend/Application.groovy
@@ -2,9 +2,23 @@ package shortcuts.backend
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
+import org.springframework.core.env.MapPropertySource
 
-class Application extends GrailsAutoConfiguration {
+class Application extends GrailsAutoConfiguration implements EnvironmentAware {
+
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
+
+    @Override
+    void setEnvironment(Environment environment) {
+        def externalConfigFile = new File('/home/tomcat/shortcuts', 'application.groovy')
+        if (externalConfigFile.exists()) {
+            def externalConfig = new ConfigSlurper().parse(externalConfigFile.toURI().toURL())
+            environment.propertySources.addFirst(new MapPropertySource("ExternalConfig", externalConfig))
+        }
+    }
+
 }
