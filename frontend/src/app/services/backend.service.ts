6c6ea872845496846ba5d3f833ff75cdb90b1f4b
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-08-15 18:49:05 +0200

    Fix server deployment issues

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -3,25 +3,40 @@ import { Injectable } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 
+const BASE_URL = environment.backendBaseUrl;
+
 @Injectable()
 export class BackendService {
-
-    private static readonly BASE_URL = environment.backendBaseUrl;
 
     constructor(
         private readonly httpClient: HttpClient,
     ) {}
 
-    get(endpointUrl: string): Promise<any> {
-        return this.httpClient.get(BackendService.fullUrl(endpointUrl)).toPromise();
+    async get(endpointUrl: string): Promise<any> {
+        const url = fullUrl(endpointUrl);
+
+        return this.httpClient.get(url)
+            .toPromise()
+            .catch(handleError);
     }
 
-    post(endpointUrl: string, body: any): Promise<any> {
-        return this.httpClient.post(BackendService.fullUrl(endpointUrl), body).toPromise();
+    async post(endpointUrl: string, body: any): Promise<any> {
+        const url = fullUrl(endpointUrl);
+
+        return this.httpClient.post(url, body)
+            .toPromise()
+            .catch(handleError);
     }
 
-    private static fullUrl(endpointUrl: string): string {
-        return `${BackendService.BASE_URL}/${endpointUrl}`;
-    }
+}
 
+function fullUrl(endpointUrl: string): string {
+    return `${BASE_URL}/${endpointUrl}`;
+}
+
+function handleError(error: Error): void {
+    // tslint:disable-next-line:no-console
+    console.error(error);
+
+    throw error;
 }
